a360cd20063362a142dcf9954d15221872d07176
Cloudlele <omikula123@gmail.com>
2018-11-25 15:48:37 +0200

    Serv21.11

--- Serv.py
+++ Serv.py
@@ -1,9 +1,17 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
 import socket
-import json, cgi
+import pypyodbc
+
 
 
 class Server(BaseHTTPRequestHandler):
+
+    def connetToDB(self):
+        connection = pypyodbc.connect(driver='{SQL Server}', server='DESKTOP-7GE22QK\SQLEXPRESS',
+                                      database='Library')
+        return connection
+
     def _set_headers(self):
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
@@ -15,13 +23,32 @@ class Server(BaseHTTPRequestHandler):
 
     # GET sends back a Hello world message
     def do_GET(self):
-        self._set_headers()
-        data = json.dumps({'hello': 'world', 'received': 'ok'})
-        self.wfile.write(bytes(str(data).encode()))
+        DataDic = dict()
+        DataMass = []
+        if self.path == '/data':
+            cursor = self.connetToDB().cursor()
+            self._set_headers()
+            SQLQuery = ("""Select Books.ID_Book, Books.Title, Books.Years, Books.Count_Download, Books.Rating, Author.Name, Author.Surname
+                        From Books
+                        Join BooksAuthor on Books.ID_Book = BooksAuthor.Code_Books
+                        Join Author on BooksAuthor.Code_Author = Author.ID_Author""")
+            cursor.execute(SQLQuery)
+            result = cursor.fetchall()
+            self.connetToDB().close()
+            for i in result:
+                DataDic['ID_Books'] = str(i[0])
+                DataDic['Title'] = str(i[1])
+                DataDic['Years'] = str(i[2])
+                DataDic['Count_Download'] = str(i[3])
+                DataDic['Rating'] = str(i[4])
+                DataDic['Author'] = str(i[5]) + ' ' + str(i[6])
+                DataMass.append(DataDic)
+                DataDic = {}
+            data = json.dumps(DataMass)
+            self.wfile.write(bytes(str(data).encode()))
 
     # POST echoes the message adding a JSON field
     def do_POST(self):
-        # ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
         content_len = int(self.headers['Content-Length'])
         post_body = self.rfile.read(content_len).decode('utf-8')
         print(post_body.strip().split())
@@ -31,28 +58,14 @@ class Server(BaseHTTPRequestHandler):
                 iter = id
             if id == iter + 1:
                 print(i)
-        # # refuse to receive non-json content
-        #
-        # # read the message and convert it into a python dictionary
-        # length = int(self.headers.getheader('content-length'))
-        # message = json.loads(self.rfile.read(length))
-        # print(message)
-        # add a property to the object, just to mess with data
-
-        # # send the message back
-        # self._set_headers()
-        # self.wfile.write(json.dumps(message))
-
 
 def run(server_class=HTTPServer, handler_class=Server, port=8080):
-    host_name = socket.gethostbyname(socket.gethostname())
-    server_address = (host_name, port)
-    print(server_address)
-    httpd = server_class(server_address, handler_class)
-
-    print('Starting httpd on port %d...' % port)
-    httpd.serve_forever()
-
+        host_name = socket.gethostbyname(socket.gethostname())
+        server_address = (host_name, port)
+        print(server_address)
+        httpd = server_class(server_address, handler_class)
+        print('Starting httpd on port %d :' % port)
+        httpd.serve_forever()
 
 if __name__ == "__main__":
     from sys import argv
